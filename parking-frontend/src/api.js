0597a3240815ba93c89293faed8cd43b3f8ebeb3
Yosef Notik <yosinotik@gmail.com>
2024-11-07 18:06:05 +0200

    Update frontend and prepare for deployment

--- parking-frontend/src/api.js
+++ parking-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://car-management.herokuapp.com', // Use the Heroku URL for production
+  baseURL: process.env.REACT_APP_API_URL, // Use the Heroku URL for production
 });
 
 export default api;
